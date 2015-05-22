aa4d76cc550a8bae6cb47297f017851b38cf6945
Collin Long <collin.r.long@utexas.edu>
2015-05-21 23:12:05 -0500

    Cleaned up the code by adding functions

--- scraper.py
+++ scraper.py
@@ -5,6 +5,52 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import re
 
+# Begin definitons of functions #
+
+# Finds the series URL specific to the Cubs game
+def find_series (series_obj):
+	for element in series_obj.find_all('li'):
+		series = str(element.string.strip())
+		if 'chnmlb' in series: return series
+
+# Parses and converts the start time of the game to CT
+def get_start_time (game_info_obj):
+	# Get the game time and convert it to CT (Time zone Cubs play in)
+	game_time = "%s%s" % (game_info_obj.game['time'], game_info_obj.game['ampm'])
+	time_format = "%s-%s-%s %s" % (year, month, day, game_time)
+	time_zone = "CT"
+	start_time_CT = datetime.strptime(time_format, "%Y-%m-%d %I:%M%p")
+	return start_time_CT.replace(hour = start_time_CT.hour - 1)
+
+# Parses the team's name and record
+def get_team_info(team, game_info_obj):
+	team_name = "%s_team_name" % team
+	wins = "%s_win" % team
+	loss = "%s_loss" % team
+	return "%s (%s-%s)" % (game_info_obj.game[team_name], game_info_obj.game[wins], game_info_obj.game[loss])
+
+# Prints/writes to file an error message if there are no highlights for the Cubs
+def error_check (current_highlights, now, start_time):
+	if "[]" in str(current_highlights.find_all(team_id = '112')):
+		if (now < start_time):
+			print("The game hasn't started yet!")
+			file.write("The game hasn't started yet!")
+		else:
+			print("The game is in progress, but we don't have any highlights yet!")
+			file.write("The game is in progress, but we don't have any highlights yet!")
+
+# Parse only highlights from the Cubs and prints them to console/writes them to file
+def get_highlights (current_highlights):
+	for element in current_highlights.find_all(team_id = '112'):
+		# Print out and save highlight name and direct url to highlight
+		highlight_string = "%s: %s\n" % (element.blurb.string, element.url.string)
+		print(highlight_string)
+		file.write("%s\n" % (highlight_string))
+
+# End definition of functions #
+
+# Begin main script #
+
 # Get current date information
 now = datetime.now()
 month = str(now.month)
@@ -22,57 +68,38 @@ if len(day) == 1:
 file_string = "highlights_%s_%s.txt" % (month, day)
 file = open(file_string, 'w')
 
-# Form the base url and search for the url specific to the Cubs game
+# Find the Cubs series URL
 base_url = "http://gdx.mlb.com/components/game/mlb/year_2015/month_%s/day_%s/" % (month, day)
 series_response = requests.get(base_url)
-current_series = BeautifulSoup(series_response.text)
+series_obj = BeautifulSoup(series_response.text)
+current_series = find_series(series_obj)
 
-for element in current_series.find_all('li'):
-	series = str(element.string.strip())
-	if 'chnmlb' in series: break
-
-# Form the gameday url and parse the gameday info
-game_url = "%s%slinescore.xml" % (base_url, series)
+# Form the URL used to parse gameday information
+game_url = "%s%slinescore.xml" % (base_url, current_series)
 game_response = requests.get(game_url)
-game_info = BeautifulSoup(game_response.text, 'xml')
+game_info_obj = BeautifulSoup(game_response.text, 'xml')
 
-# Get the starting time and convert it to CT (Time zone Cubs play in)
-game_time = "%s%s" % (game_info.game['time'], game_info.game['ampm'])
-time_format = "%s-%s-%s %s" % (year, month, day, game_time)
+# Parse the starting time and team info
+start_time = get_start_time (game_info_obj)
 time_zone = "CT"
-start_time_CT = datetime.strptime(time_format, "%Y-%m-%d %I:%M%p")
-start_time_CT = start_time_CT.replace(hour = start_time_CT.hour - 1)
+away = get_team_info('away', game_info_obj)
+home = get_team_info('home', game_info_obj)
 
+# Print/write to file the gameday info
+print("Gameday:\n%s/%s %s @ %s %s %s\n" % (month, day, away, home, start_time.strftime("%I:%M%p"), time_zone))
+file.write("Gameday:\n%s/%s %s @ %s %s %s\n\n" % (month, day, away, home, start_time.strftime("%I:%M%p"), time_zone))
 
-away = "%s (%s-%s)" % (game_info.game['away_team_name'], game_info.game['away_win'], game_info.game['away_loss'])
-home = "%s (%s-%s)" % (game_info.game['home_team_name'], game_info.game['home_win'], game_info.game['home_loss'])
-
-# Print and save gameday info
-print("Gameday:\n%s/%s %s @ %s %s %s\n" % (month, day, away, home, start_time_CT.strftime("%I:%M%p"), time_zone))
-file.write("Gameday:\n%s/%s %s @ %s %s %s\n\n" % (month, day, away, home, start_time_CT.strftime("%I:%M%p"), time_zone))
-
-# Form the highlights url and parse the highlights
-index_url = "%s%smedia/highlights.xml" % (base_url, series)
+# Form the URL used to parse the highlights
+index_url = "%s%smedia/highlights.xml" % (base_url, current_series)
 highlights_response = requests.get(index_url)
 current_highlights = BeautifulSoup(highlights_response.text, 'xml')
 
+# Print/write to file all the available Cubs highlights
 print("Highlights:")
 file.write("Highlights:\n")
-
-# Error check if there are no highlights
-if "[]" in str(current_highlights.find_all(team_id = '112')):
-	if (now < start_time_CT):
-		print("The game hasn't started yet!")
-		file.write("The game hasn't started yet!")
-	else:
-		print("The game is in progress, but we don't have any highlights yet!")
-		file.write("The game is in progress, but we don't have any highlights yet!")
-
-# Search by team_id to filter by Cubs highlights only
-for element in current_highlights.find_all(team_id = '112'):
-	# Print out and save highlight name and direct url to highlight
-	highlight_string = "%s: %s\n" % (element.blurb.string, element.url.string)
-	print(highlight_string)
-	file.write("%s\n" % (highlight_string))
+error_check(current_highlights, now, start_time)
+get_highlights(current_highlights)
 
 file.close()
+
+# End main script #
